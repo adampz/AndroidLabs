51a56e5a7b6cc6df1c9c1488fc669eeac891b4c1
adampz <adamplaterzyberk@gmail.com>
2021-02-10 19:01:36 -0500

    step 1

--- app/src/main/java/com/cst2335/plat0039/MainActivity.java
+++ app/src/main/java/com/cst2335/plat0039/MainActivity.java
@@ -16,7 +16,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_relative);
+        setContentView(R.layout.activity_main_linear);
 
         Button btn = findViewById((R.id.button));
         btn.setOnClickListener( clk -> {
